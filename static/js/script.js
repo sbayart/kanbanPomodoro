5f1b8efc779e54e4e9d848e22a5f6e0b3d3890e8
karina BRANTE <karina.brante@gmail.com>
2017-03-02 17:08:32 +0100

    je craqueeeee

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,26 @@
 $(document).ready(function() {
-    $("#add").click(function(){
+    $("#add").click(function() {
         var texte = "";
         texte = $("#text").val();
         var monli = $("<li></li>");
         monli.appendTo($("#todo"));
-        monli.html(texte+"<span class='glyphicon glyphicon-remove-sign' aria-hidden='true' id='suppr'></span>");
+        monli.html(texte + "<span class='glyphicon glyphicon-remove-sign' aria-hidden='true' ></span>");
+        $(".glyphicon-remove-sign").click(function() {
+            $(this).parent().remove();
+        })
+        $(monli).click(function() {
+            $("#encours").append(monli)
+            $('.timer').startTimer()
+            if (".timer" == onComplete()) {
+                alert("yolo")
+            }
+            console.log($(".timer"));
+        })
     });
-    $("#suppr").click(function(){
-        $("li").remove();
-    })
 
-    $(function(){
-       $('.timer').startTimer();
-     });
- })
+
+    function onComplete() {
+        $(".timer").addClass('jst-timeout');
+
+    }
+})
