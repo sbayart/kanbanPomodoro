f12137e99fa9648a98c5546a79f0d7e6318fa4be
karina BRANTE <karina.brante@gmail.com>
2017-03-02 15:29:45 +0100

    debut js

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+    $("#add").click(function(){
+        var texte = "";
+        texte = $("#text").val();
+        var monli = $("<li></li>");
+        monli.appendTo($("#todo"));
+        monli.html(texte+"<span class='glyphicon glyphicon-remove-sign' aria-hidden='true'></span>");
+    });
+    $(".glyphicon-remove-sign").click(function(){
+        todo.remove(this)
+    })
+
     $(function(){
        $('.timer').startTimer();
      });
