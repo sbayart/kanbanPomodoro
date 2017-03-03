9fa898f969982facc7d7662fc3a1f0f6356148a9
karina BRANTE <karina.brante@gmail.com>
2017-03-03 12:10:34 +0100

    changement de timer

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,31 @@
 $(document).ready(function() {
+    var monli = "";
     $("#add").click(function() {
         var texte = "";
         texte = $("#text").val();
-        var monli = $("<li></li>");
+        monli = $("<li></li>");
         monli.appendTo($("#todo"));
         monli.html(texte + "<span class='glyphicon glyphicon-remove-sign' aria-hidden='true' ></span>");
         $(".glyphicon-remove-sign").click(function() {
             $(this).parent().remove();
         })
-        $(monli).click(function() {
-            $("#encours").append(monli)
+        monli.click(function() {
+            $("#encours").append($(this))
         });
-        $("#start").click(function() {
-            $('.timer').startTimer({
-                onComplete: function(element){
-                    element.addClass('is-complete');
-                    var travail = confirm("Avez-vous fini ?");
-                        if (travail == true) {
-                            $("#fait").append(monli);
-                            $(".timer").resetTimer(element);
-                        } else {
-                            $('.timer').startTimer(element);
-                        }
-                }
-            })
+    })
+                $('#clock').countdown('2017/03/04 00:00:05', function(event) {
+            $(this).html(event.strftime(' %H:%M:%S'));
+            });
+
+                //     var travail = confirm("Avez-vous fini ?");
+                //     if (travail == true) {
+                //         $("#fait").append(monli);
+                //         $(".timer").resetTimer(element);
+                //     } else {
+                //         $('.timer').startTimer(element);
+                //     }
+                // })
             $("#pause").click(function(){
 
             })
-        })
-    });
-})
+});
